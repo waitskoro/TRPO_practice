028cf8ac84da699f3702dbaaf2f6109759d6c7cc
Гурьянова Кристина Петровна <gr604_gukpe@ttit.local>
2023-02-28 12:10:55 +0700

    number_2

--- TRPO_practice/View/Autorization.xaml.cs
+++ TRPO_practice/View/Autorization.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TRPO_practice
+namespace TRPO_practice.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Autorization.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Autorization : Window
     {
-        public MainWindow()
+        public Autorization()
         {
             InitializeComponent();
         }
